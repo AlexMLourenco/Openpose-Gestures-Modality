cdcae17eb118d4a26fe6504141c2bd203b32ae77
ManuelCapao <manuel.capao@ua.pt>
2020-05-14 15:13:30 +0100

    distance calculator

--- test.py
+++ test.py
@@ -5,11 +5,44 @@ from dtaidistance import dtw
 from dtaidistance import dtw_visualisation as dtwvis
 import numpy as np
 
+
+#------------------------ Keypoints variation (x , y) --------------------""
+def get_keypoints(_array):
+
+    keypoints_pose = list({i:{"x":[],"y":[]}} for i in range(len(_array[0])//3))
+    for item in _array:
+        for k in range(len(keypoints_pose)):
+            j = k*3 + 1
+            keypoints_pose[k][k]["x"].append(item[k*3])
+            keypoints_pose[k][k]["y"].append(item[j])
+    return keypoints_pose
+#-------------- Calc distances -------------------------------------#
+def get_distances(keypoints1, keypoints2):
+
+    distances = list({i:{"dist_x":[],"dist_y":[]}} for i in range(len(keypoints1)))
+
+    for i in range(len(distances)):
+        distances[i][i]["dist_x"].append(dtw.distance(keypoints1[i][i]["x"], keypoints2[i][i]["x"])) 
+        distances[i][i]["dist_y"].append(dtw.distance(keypoints1[i][i]["y"], keypoints2[i][i]["y"]))
+
+    return distances
+
+
+#--------------------------- MAIN-------------------------------------#
+
+
 file_name = ".\\json\\" + sys.argv[1]
+file2_name = ".\\json\\" + sys.argv[2]
 
 pose_arr = []
 hand_left = []
 hand_right = []
+pose_arr2 = []
+hand_left2 = []
+hand_right2 = []
+
+
+
 
 with open(file_name) as json_file:
     data = json.load(json_file)
@@ -19,20 +52,19 @@ with open(file_name) as json_file:
         hand_left.append(data["data"][key][0]["hand_left_keypoints_2d"])
         hand_right.append(data["data"][key][0]["hand_right_keypoints_2d"])
 
-    print("POSE ARRAY")
-    print(pose_arr)
-    keypoint1_x = [i[0] for i in pose_arr]
-    keypoint1_y = [i[1] for i in pose_arr]
+with open(file2_name) as json_file:
+    data = json.load(json_file)
 
-print(keypoint1_y)
-print(keypoint1_x)
-# path = dtw.warping_path(s1, s2)
-# dtwvis.plot_warping(s1, s2, path, filename="warp.png")
-# s1 = [0, 0, 1, 2, 1, 0, 1, 0, 0]
-# s2 = [0, 1, 2, 0, 0, 0, 0, 0, 0]
-dist = dtw.distance(keypoint1_x, keypoint1_x)
-print(dist)
-# print("HAND LEFT")
-# print(hand_left)
-# print("HAND_RIGHT")
-# print(hand_right)
+    for key in data["data"]:
+        pose_arr2.append(data["data"][key][0]["pose_keypoints_2d"])
+        hand_left2.append(data["data"][key][0]["hand_left_keypoints_2d"])
+        hand_right2.append(data["data"][key][0]["hand_right_keypoints_2d"])
+
+ 
+    
+    keypoints_pose_1 = get_keypoints(pose_arr)
+    keypoints_pose_2 = get_keypoints(pose_arr2)
+    #keypoints_left_hand = get_keypoints(hand_left)
+    #keypoints_right_hand = get_keypoints(hand_right)
+    dist = get_distances(keypoints_pose_1,keypoints_pose_2)
+    print(dist)
