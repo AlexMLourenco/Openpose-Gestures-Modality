554a271d04540c3694c9fd3422b72a2325121d7a
Manuel Santos <manuel.capao@ua.pt>
2020-05-28 21:08:48 +0100

    euclidian distance

--- compareTest.py
+++ compareTest.py
@@ -11,7 +11,7 @@ from os.path import isfile, join
 from test import get_distances, get_keypoints
 
 #---------------------Euclidian--------------------------------#
-def get_euclidian(points):
+def get_euclidian(points,points2,id):
     
     fk = {}
     for keypoint in range(len(points)):  
@@ -22,13 +22,32 @@ def get_euclidian(points):
                     f0k0 = [points[keypoint][frame]["x"][key], points[keypoint][frame]["y"][key]]
                     d0.append(f0k0)
                     fk[frame] = d0
-                
-    x = np.array(fk)
 
+    fk2 = {}
+    for keypoint in range(len(points2)):  
+        for frame in points2[keypoint]:
+            d0 = []
+            for key in range(len(points2[keypoint][frame]["x"])):
+                if(points2[keypoint][frame]["x"][key] != 0 or points2[keypoint][frame]["x"][key] != 0):
+                    f0k0 = [points2[keypoint][frame]["x"][key], points2[keypoint][frame]["y"][key]]
+                    d0.append(f0k0)
+            fk2[frame] = d0
+                
+    dist = {label: 0}
+    distan = 0
+    for key in range(len(fk)):
+        x = fk[key]
+        #print(x)
+        y = fk2[key]
+        distance, path = fastdtw(x,y, dist=euclidean)
+        distan += distance
+        
+    dist[label] = distan/20
     #distance, path = fastdtw(x, y, dist=euclidian)
     #print(distance)
-
-    print(x)
+    return distan/20
+    #print("primeiro:" + str(fk))
+    #print("segundo:" + str(fk2))
 #--------------------------- MAIN-------------------------------------#
 file_name = "./json/" + sys.argv[1] + ".json"
 mypath = "./json/"
@@ -44,27 +63,49 @@ with open(file_name) as json_file:
     data = json.load(json_file)
 
     for key in data["data"]:
-        hand_right.append(data["data"][key][0]["hand_left_keypoints_2d"])
+        hand_right.append(data["data"][key][0]["hand_right_keypoints_2d"])
 
     keypoints_right_1 = get_keypoints(hand_right)
 
-    get_euclidian(keypoints_right_1)
-
+    
     #print(keypoints_right_1)
-     
+d = {}
+di = []
 for f in onlyfiles:
     with open("./json/"+f) as json_file:
         data = json.load(json_file)
 
         hand_right = []
         for key in data["data"]:
-            hand_right.append(data["data"][key][0]["hand_left_keypoints_2d"])
             label = data["label"]
             identifier = data["id"]
-
+            
+            print(data["data"][key][0]["hand_right_keypoints_2d"])
+            hand_right.append(data["data"][key][0]["hand_right_keypoints_2d"])
+            
+        di.append({"label": label, "identifier": identifier})
         keypoints_right_2 = get_keypoints(hand_right)
-        #print(label)
-        #print(identifier)
-
+       
+        print(identifier + "\n")
+        print(label + "\n")
         dist_right = get_distances(keypoints_right_1,keypoints_right_2)
         #print(dist_right)
+    
+    dist = get_euclidian(keypoints_right_1,keypoints_right_2,identifier)
+    if dist > 0:
+        d[identifier] = dist
+
+print(d)
+a = []
+
+key_list = list(d.keys()) 
+val_list = list(d.values()) 
+    
+#print(key_list[val_list.index(min(val_list))]) 
+for item in di:
+    if item["identifier"] == key_list[val_list.index(min(val_list))]:
+        print("O gesto que tem menor distancia é: " + item["label"])
+        break
+
+
+    
